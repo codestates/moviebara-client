14b3cb76a2860c6ac9edb0414d895e40d00be9b9
MUNA <munawiki@gmail.com>
2021-02-03 11:47:05 +0900

    router 스켈레톤 구현

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import ReactDOM from "react-dom";
 import React from "react";
 import App from "./components/App.js";
-import reportWebVitals from "./reportWebVitals.js";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,4 +12,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals(console.log);
