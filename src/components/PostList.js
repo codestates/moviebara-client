49103e00662f7417f78ce487cc4a39f374b77397
ssdd33 <ojjonino@ojjoninoui-MacBookPro.local>
2021-02-05 20:24:28 +0900

    modified postList props name

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,10 +1,10 @@
 import styles from "../css/postlist.module.css";
 import Post from "./Post.js";
 
-export default function PostList({ mvPosts }) {
+export default function PostList({ posts }) {
   return (
     <div className={styles.reviewbox}>
-      {mvPosts.map((p) => (
+      {posts.map((p) => (
         <Post key={p.id} post={p} />
       ))}
     </div>
