b79facf871df0f6841c88597dd4da19578b759d9
ssdd33 <ojjonino@ojjoninoui-MacBookPro.local>
2021-02-12 20:04:41 +0900

    connect localhost

--- src/components/Userhome.js
+++ src/components/Userhome.js
@@ -1,226 +1,224 @@
-// import {
-//   Switch,
-//   Route,
-//   Link,
-//   useRouteMatch,
-//   useParams,
-// } from "react-router-dom";
-// import styles from "../css/userhome.module.css";
-// import PostList from "./PostList";
+import {
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams,
+  useHistory,
+} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import styles from "../css/userhome.module.css";
+import PostList from "./PostList";
+import UpdateReviewBox from "./UpdateReviewBox";
 
-// import UpdateReviewBox from "./UpdateReviewBox";
+export default function Userhome({ userInfo }) {
+  const { id, nickname, image } = userInfo;
+  const match = useRouteMatch();
+  const [posts, setPosts] = useState(null);
+  const [scraps, setScraps] = useState(null);
+  const [scraps_id, setScraps_id] = useState(null);
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [filtered, setFiltered] = useState(null);
+  const [genres, setGenres] = useState(null);
+  const history = useHistory();
 
-// // with dummyData
-// //import PostListUD from "./PostListUD";
-// // import posts from "../dummy/posts.json";
-// // import users from "../dummy/users.json";
-// // import movies from "../dummy/movies.json";
-// // const usersData = users;
-// // const postsData = posts;
-// // const movieData = movies.movies;
+  const getGenreArray = (reviews) => {
+    const temp = reviews.map((p) => p.movie.genre);
 
-// // export default function Userhome() {
-// //   const user = usersData.users[0];
-// //   const scrapPostId = [5, 6, 7, 8, 9];
-// //   const userPosts = postsData.posts.filter((post) => post.user_id === user.id);
-// //   const scrapPosts = postsData.posts.filter((post) =>
-// //     scrapPostId.includes(post.id)
-// //   );
-// //   let match = useRouteMatch();
+    setGenres([...new Set(temp)]);
+  };
 
-// //   return (
-// //     <div className={styles.container}>
-// //       <div className={styles.userInfo}>
-// //         <img className={styles.img} src={user.image} />
-// //         <div className={styles.info}>
-// //           <div>{user.nickname}</div>
-// //           <div>
-// //             내가 쓴 리뷰 : {userPosts.length} 스크랩한 리뷰 :{" "}
-// //             {scrapPosts.length}
-// //           </div>
-// //         </div>
-// //       </div>
-// //       <div className={styles.linksBox}>
-// //         <Link to={match.url}>내가 쓴 리뷰</Link>
-// //         <Link to={`${match.url}/scraps`}>스크랩한 리뷰</Link>
-// //       </div>
-// //       <div className={styles.postsBox}>
-// //         <Switch>
-// //           <Route exact path={match.path}>
-// //             <PostListUD posts={userPosts} />
-// //           </Route>
-// //           <Route path={`${match.path}/scraps`}>
-// //             <PostList posts={scrapPosts} />
-// //           </Route>
-// //           <Route path={`${match.path}/:id`}>
-// //             <UpdateReviewBox />
-// //           </Route>
-// //         </Switch>
-// //       </div>
-// //     </div>
-// //   );
-// // }
+  const getPosts = async () => {
+    try {
+      // 요청이 시작 할 때에는 error 와 users 를 초기화하고
+      setError(null);
+      setPosts(null);
+      setScraps(null);
+      setScraps_id(null);
+      setData(null);
 
-// // with state
+      // loading 상태를 true 로 바꿉니다.
+      setLoading(true);
+      const response = await axios.get(
+        `http://localhost:4000/posts?user_id=${id}`
+      );
+      const response2 = await axios.get(
+        `http://localhost:4000/scraps?user_id=${id}`
+      );
 
-// import React, { useEffect, useState } from "react";
-// import axios from "axios";
+      setPosts(response.data.post);
+      setData(response.data.post);
+      setScraps(response2.data.data);
+      const scrapIds = response2.data.data.map((p) => p.postId);
+      setScraps_id(scrapIds);
+    } catch (e) {
+      setError(e);
+    }
 
-// export default function Userhome({ userInfo, scraps, movies, accessToken }) {
-//   const { userId, nickname, image } = userInfo;
+    setLoading(false);
+  };
 
-//   const [posts, setPosts] = useState(null);
-//   const [genres, setGenres] = useState(null);
-//   const [Rmessage, setRMessage] = useState("img url");
-//   const [Smessage, setSMessage] = useState("img url");
-//   const [filtered, setFiltered] = useState(posts);
+  const filtPosts = (genre) => {
+    const filteredPosts = posts.filter((p) => p.movie.genre === genre);
 
-//   const { scrap } = useParams();
+    setFiltered(filteredPosts);
+  };
 
-//   useEffect(() => {
-//     if (!scrap) {
-//       getPosts();
-//     } else {
-//       setPosts(scraps);
-//       if (scraps.length === 0) {
-//         setSMessage("Scrap reviews!");
-//       }
-//     }
-//     getGenreArray(posts);
-//   });
+  useEffect(() => {
+    getPosts();
+  }, []);
 
-//   const getPosts = () => {
-//     axios
-//       .get(`https://api.moviebara.com/posts?user_id=${userId}`, {
-//         headers: {
-//           Authorization: `Bearer ${accessToken}`,
-//           "Content-Type": "text/plain",
-//         },
-//         withCredentials: true,
-//       })
-//       .then((res) => {
-//         if (res.status === 200) {
-//           setPosts(res.posts);
-//         } else if (res.data.message === "There's no post of user") {
-//           setMessage("Post your first review!");
-//         } else if (res.data.message === "need some id") {
-//           setMessage("please login again");
-//         }
-//       });
-//   };
+  const handleDelete = (postId) => {
+    console.log("delete from userhome");
+    const data = JSON.stringify({ postId: postId });
+    const config = {
+      method: "delete",
+      url: "http://localhost:4000/posts/",
+      headers: {
+        "Content-Type": "application/json",
+      },
 
-//   const getGenreArray = (reviews) => {
-//     const temp = reviews.map((p) => {
-//       for (let movie of movies) {
-//         if (p.movie_id === movie.id) return movie.genre;
-//       }
-//     });
+      data,
+    };
+    axios(config).then((res) => {
+      axios
+        .get(`http://localhost:4000/posts?user_id=${id}`)
+        .then((res) => {
+          console.log("what the ffffffffffffffffff");
+          setPosts(res.data.post);
+          setData(res.data.post);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    });
+  };
 
-//     setGenres([...new Set(temp)]);
-//   };
+  const handleScrap = (postId) => {
+    const data = JSON.stringify({ postId: postId, userId: id });
+    const config = {
+      method: "post",
+      url: "http://localhost:4000/scraps/",
+      headers: {
+        "Content-Type": "application/json",
+      },
 
-//   const filtPosts = (genre) => {
-//     const filteredPosts = posts.filter((p) => {
-//       const mvGenres = movies.filter((mv) => {
-//         if (mv.id === p.movie_id) {
-//           return mv.genre;
-//         }
-//       });
-//       return mvGenres.includes(genre);
-//     });
+      data,
+    };
+    axios(config).then((res) => {
+      axios
+        .get(`http://localhost:4000/scraps?user_id=${id}`)
+        .then((res) => {
+          console.log("what the ffffffffffffffffff");
+          setScraps(res.data.data);
+          const scrapIds = res.data.data.map((p) => p.postId);
+          setScraps_id(scrapIds);
+          setData(res.data.data);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    });
+  };
+  const handleUnscrap = (postId) => {
+    const data = JSON.stringify({ postId: postId, userId: id });
+    const config = {
+      method: "delete",
+      url: "http://localhost:4000/scraps/",
+      headers: {
+        "Content-Type": "application/json",
+      },
 
-//     setFiltered(filteredPosts);
-//   };
+      data,
+    };
+    axios(config).then((res) => {
+      axios
+        .get(`http://localhost:4000/scraps?user_id=${id}`)
+        .then((res) => {
+          console.log("what the ffffffffffffffffff");
+          setScraps(res.data.data);
+          const scrapIds = res.data.data.map((p) => p.postId);
+          setScraps_id(scrapIds);
+          setData(res.data.data);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    });
+  };
 
-//   const handleDelete = (postId) => {
-//     axios.delete(
-//       `https://api.moviebara.com/posts`,
-//       { postId },
-//       {
-//         headers: {
-//           Authorization: `Bearer ${accessToken}`,
-//           "Content-Type": "text/plain",
-//         },
-//         withCredentials: true,
-//       }
-//     );
-//   };
+  if (loading) return <div>로딩중..</div>;
+  if (error) return <div>에러 발생!!{error}</div>;
+  if (!posts && !scraps) return null;
+  return (
+    <div className={styles.container}>
+      <div className={styles.userInfo_box}>
+        <div className={styles.img_box}>
+          <img className={styles.img} src={image} alt="사용자 프로필 사진" />
+        </div>
+        <div className={styles.info_box}>
+          <p>{nickname}</p>
+          <p>
+            내가 쓴 리뷰 : {posts.length} 스크랩 : {scraps.length}
+          </p>
+        </div>
+      </div>
 
-//   return (
-//     <div className={styles.container}>
-//       <div className={styles.userInfo_box}>
-//         <div className={styles.img_box}>
-//           <img className={styles.img} src={image} alt="사용자 프로필 사진" />
-//         </div>
-//         <div className={styles.info_box}>
-//           <p>{nickname}</p>
-//           <p>
-//             내가 쓴 리뷰 : {posts.length} 스크랩 : {scraps.length}
-//           </p>
-//         </div>
-//       </div>
-
-//       <div className={styles.links_Box}>
-//         <div className={styles.myReview_link}>
-//           <Link to={match.url}>내가 쓴 리뷰</Link>
-//         </div>
-//         <div className={styles.myScrap_link}>
-//           <Link to={`${match.url}/scraps`}>스크랩</Link>
-//         </div>
-//       </div>
-//       <Switch>
-//         <Route exact path={match.path}>
-//           <div className={styles.genreFilter_box}>
-//             <div className={styles.genre_tag} onClick={setFiltered(posts)}>
-//               모든 장르
-//             </div>
-//             {genres.map((g) => (
-//               <div
-//                 key={genres.indexOf(g)}
-//                 className={styles.genre_tag}
-//                 onClick={filtPosts(g)}
-//               >
-//                 {g}
-//               </div>
-//             ))}
-//           </div>
-//           <div className={styles.posts_box}>
-//             <PostList
-//               posts={filtered}
-//               userInfo={userInfo}
-//               scraps={scraps}
-//               deleteHandler={handleDelete}
-//             />
-//           </div>
-//         </Route>
-//         <Route path={`${match.path}/scraps`}>
-//           <div className={styles.genreFilter_box}>
-//             <div className={styles.genre_tag} onClick={setFiltered(scraps)}>
-//               모든 장르
-//             </div>
-//             {genres.map((g) => (
-//               <div
-//                 key={genres.indexOf(g)}
-//                 className={styles.genre_tag}
-//                 onClick={filtPosts(g)}
-//               >
-//                 {g}
-//               </div>
-//             ))}
-//           </div>
-//           <div className={styles.posts_box}>
-//             <PostList
-//               posts={filtered}
-//               userInfo={userInfo}
-//               scraps={scraps}
-//               deleteHandler={handleDelete}
-//             />
-//           </div>
-//         </Route>
-//         <Route path={`${match.path}/:userId/:movieId`}>
-//           <UpdateReviewBox />
-//         </Route>
-//       </Switch>
-//     </div>
-//   );
-// }
+      <div className={styles.links_Box}>
+        <div className={styles.myReview_link}>
+          <button
+            onClick={() => {
+              setData(posts);
+            }}
+          >
+            내가 쓴 리뷰
+          </button>
+        </div>
+        <div className={styles.myScrap_link}>
+          <button
+            onClick={() => {
+              setData(scraps);
+              console.log(scraps_id);
+            }}
+          >
+            스크랩
+          </button>
+        </div>
+      </div>
+      <Switch>
+        <Route exact path={match.path}>
+          {/* <div className={styles.genreFilter_box}>
+            <div className={styles.genre_tag} onClick={setFiltered(posts)}>
+              모든 장르
+            </div>
+            {genres.map((g) => (
+              <div
+                key={genres.indexOf(g)}
+                className={styles.genre_tag}
+                onClick={filtPosts(g, posts)}
+              >
+                {g}
+              </div>
+            ))}
+          </div> */}
+          <div className={styles.posts_box}>
+            <PostList
+              posts={data}
+              userInfo={userInfo}
+              scraps_id={scraps_id}
+              deleteHandler={handleDelete}
+              scrapHandler={handleScrap}
+              unscrapHandler={handleUnscrap}
+            />
+          </div>
+        </Route>
+        <Route path={`${match.path}/:postId`}>
+          <UpdateReviewBox />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
