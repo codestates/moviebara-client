785f116734a190b2aac7393ab396a3812f0ff4dd
MUNA <munawiki@gmail.com>
2021-02-13 10:22:29 +0900

    material-ui install

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCog } from "@fortawesome/free-solid-svg-icons";
-import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
 import styles from "../css/header.module.css";
 
 function Header({ isLogin, userInfo }) {
